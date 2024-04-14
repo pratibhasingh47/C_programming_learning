b6275d2e5d2931676fcd87bbf89aaa936ac5bbaa
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-04-14 20:04:23 +0530

    change

---
+++ basic_function_demonstration & conditional_statements/basic_problem_solving_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("Enter the values\n");
+    scanf("%d\n%d\n%d", &a, &b, &c);
+    if (a > b && a > c)
+    {
+        printf("%d", a);
+    }
+    else if (b > a && b > c)
+    {
+        printf("%d", b);
+    }
+    else if (c > a && c > b)
+    {
+        printf("%d", c);
+    }
+}
