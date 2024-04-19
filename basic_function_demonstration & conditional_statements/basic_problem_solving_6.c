400b049563ffa7af41aa27bb8055681eb24eedd8
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-04-19 16:01:28 +0530

    change

---
+++ basic_function_demonstration & conditional_statements/basic_problem_solving_6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	int a;
+	printf("Enter the month no.\n");
+	scanf("%d", &a);
+
+	if (a == 1 || a == 3 || a == 5 || a == 7 || a == 8 || a == 10 || a == 12)
+	{
+		printf("No. of days is 31");
+	}
+	else if (a == 2)
+	{
+		printf("No. of days is 28");
+	}
+
+	else if (a == 4 || a == 6 || a == 9 || a == 11)
+	{
+		printf("No. of days is 30");
+	}
+}
