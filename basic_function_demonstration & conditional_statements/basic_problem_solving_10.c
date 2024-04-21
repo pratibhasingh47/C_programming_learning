96d5bf2ac8d07a1ee4ca41ae05bb27b3a67e0cfc
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-04-21 16:37:54 +0530

    c

---
+++ basic_function_demonstration & conditional_statements/basic_problem_solving_10.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the week number\n");
+    scanf("\n%d", &n);
+    switch (n)
+    {
+    case 1:
+    {
+        printf("SUNDAY");
+    }
+    case 2:
+    {
+        printf("MONDAY");
+        break;
+    }
+    case 3:
+    {
+        printf("TUESDAY");
+        break;
+    }
+    case 4:
+    {
+        printf("WEDNESDAY");
+        break;
+    }
+    case 5:
+    {
+        printf("THURSDAY");
+        break;
+    }
+    case 6:
+    {
+        printf("FRIDAY");
+        break;
+    }
+    case 7:
+    {
+        printf("SATURDAY");
+        break;
+    }
+    default:
+    {
+        printf("INVALID NO.");
+    }
+    }
+}
