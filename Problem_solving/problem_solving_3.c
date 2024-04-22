e393d2357b053b5354210b392f9fa38c1eb6c9f1
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-04-22 14:36:52 +0530

    c

--- Problem_solving/problem_solving_3.c
+++ Problem_solving/problem_solving_3.c
@@ -1,31 +1,30 @@
-#include<stdio.h>
+// WAP to check whether a number is even or odd. If the number is even then check whether number is divisible by 3 and also by 2 or not. If the number is odd then check whether the number is divisible by 5 or not. Display messages accordingly.
+
+#include <stdio.h>
 void oddeven(int a);
 int main()
 {
     int a;
     printf("Enter a value\n");
-    scanf("\n%d",&a);
+    scanf("\n%d", &a);
     oddeven(a);
-
 }
 void oddeven(int a)
 {
-    if (a%2==0)
+    if (a % 2 == 0)
     {
         printf("Even");
-        if (a%2==0 && a%3==0)
+        if (a % 2 == 0 && a % 3 == 0)
         {
             printf("\nNo. is divisible by both 2 and 3");
         }
-        
     }
-    else{
+    else
+    {
         printf("Odd");
-        if (a%5==0)
+        if (a % 5 == 0)
         {
             printf("\nNo. is divisible by 5");
         }
-        
     }
-    
 }
