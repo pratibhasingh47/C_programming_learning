75f1625b59a61cf42fb53382873a06699950135c
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-04-22 14:34:33 +0530

    change

--- Problem_solving/problem_solving_1.c
+++ Problem_solving/problem_solving_1.c
@@ -1,22 +1,24 @@
-#include<stdio.h>
+// WAP to find the greatest among the two given numbers. Also, check whether the greatest number is between 500-1000 or not. If yes, then print the value of entered number.
+
+#include <stdio.h>
 int main()
 {
     int a, b;
     printf("Enter the values\n");
-    scanf("%d\n%d",&a,&b);
-    if (a>b)
+    scanf("%d\n%d", &a, &b);
+    if (a > b)
     {
-        printf("%d is greater",a);
-        if (a>=500 && a<=1000)
+        printf("%d is greater", a);
+        if (a >= 500 && a <= 1000)
         {
             printf("Entered no. lies between 500-1000");
         }
-        
     }
-    else{
-        printf("%d is greater",b);
-        if(b>=500 && b<=1000);
+    else
+    {
+        printf("%d is greater", b);
+        if (b >= 500 && b <= 1000)
+            ;
         printf("Entered no. lies between 500-1000");
     }
-    
 }
