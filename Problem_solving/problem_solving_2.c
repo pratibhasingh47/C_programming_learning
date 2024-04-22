e5760975846b0c87635288bdaf7a760056b3420d
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-04-22 14:36:04 +0530

    c

--- Problem_solving/problem_solving_2.c
+++ Problem_solving/problem_solving_2.c
@@ -1,19 +1,21 @@
-#include<stdio.h>
+// Write a program to take marks of 5 subjects from the user. Assume marks are given out of 100 and passing marks is 33. Now display whether the candidate passed the examination or failed. Print student’s total marks only if the student has passed the examination, otherwise not.
+
+#include <stdio.h>
 int main()
 {
-    int a,b,c,d,e,x;
+    int a, b, c, d, e, x;
     printf("Enter the marks of five subjects\n");
-    scanf("%d\n%d\n%d\n%d\n%d",&a,&b,&c,&d,&e);
-    x= ((a+b+c+d+e)*100)/500;
-    if (x>=33)
+    scanf("%d\n%d\n%d\n%d\n%d", &a, &b, &c, &d, &e);
+    x = ((a + b + c + d + e) * 100) / 500;
+    if (x >= 33)
     {
         printf("PASSED\n");
         int z;
-        z=a+b+c+d+e;
-        printf("\nStudent total marks is %d",z);
+        z = a + b + c + d + e;
+        printf("\nStudent total marks is %d", z);
     }
-    else{
+    else
+    {
         printf("FAILED");
     }
-    
 }
