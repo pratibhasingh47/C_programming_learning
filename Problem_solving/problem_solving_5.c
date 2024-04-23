4ca88da2270f797b1dd031cf41967880e43aa1c7
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-04-23 21:22:32 +0530

    problem_solving

---
+++ Problem_solving/problem_solving_5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int l;
+    printf("Enter the year\n");
+    scanf("\n%d",&l);
+    if (l%4==0)
+    {
+        printf("\nIt is a leap year");
+    }
+    else{printf("\nIt is not a leap year");}
+    
+}
+
