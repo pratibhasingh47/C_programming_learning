56fe36ecd66abcc14aa597733ef9d49906e891ae
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-04-23 21:21:49 +0530

    problem_solving

---
+++ Problem_solving/problem_solving_4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the value\n");
+    scanf("\n%d",&a);
+    if(a>0)
+    {
+        printf("\nPOSITIVE");
+        if(a%10==0)
+       { printf("\nNo. is divisible by 10");}
+
+    }
+    else if (a==0)
+    {
+        printf("ZERO");
+    }
+    else if (a<0)
+    {
+        printf("NEGATIVE");
+    }
+    
+    
+}
