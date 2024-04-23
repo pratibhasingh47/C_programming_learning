be9a6bc4df6a164086937e99973a92d6584f81af
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-04-23 21:26:12 +0530

    problem_solving

--- Problem_solving/problem_solving_4.c
+++ Problem_solving/problem_solving_4.c
@@ -1,24 +1,25 @@
-#include<stdio.h>
+// Write a program to check whether a given number is positive, negative or zero. If the number is positive, check the number is divisible by 10 or not.
+
+#include <stdio.h>
 int main()
 {
     int a;
     printf("Enter the value\n");
-    scanf("\n%d",&a);
-    if(a>0)
+    scanf("\n%d", &a);
+    if (a > 0)
     {
         printf("\nPOSITIVE");
-        if(a%10==0)
-       { printf("\nNo. is divisible by 10");}
-
+        if (a % 10 == 0)
+        {
+            printf("\nNo. is divisible by 10");
+        }
     }
-    else if (a==0)
+    else if (a == 0)
     {
         printf("ZERO");
     }
-    else if (a<0)
+    else if (a < 0)
     {
         printf("NEGATIVE");
     }
-    
-    
 }
