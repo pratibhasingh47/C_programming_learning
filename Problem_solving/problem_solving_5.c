c8c2a4cb2f0c2efd7fe8a38c0ed560d3b055fbcd
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-04-23 21:27:06 +0530

    problem_solving

--- Problem_solving/problem_solving_5.c
+++ Problem_solving/problem_solving_5.c
@@ -1,14 +1,17 @@
-#include<stdio.h>
+// Write a program to check whether a year is a leap year or not
+
+#include <stdio.h>
 int main()
 {
     int l;
     printf("Enter the year\n");
-    scanf("\n%d",&l);
-    if (l%4==0)
+    scanf("\n%d", &l);
+    if (l % 4 == 0)
     {
         printf("\nIt is a leap year");
     }
-    else{printf("\nIt is not a leap year");}
-    
+    else
+    {
+        printf("\nIt is not a leap year");
+    }
 }
-
