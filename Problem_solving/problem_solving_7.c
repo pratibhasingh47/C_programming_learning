c58f03e4be862bd155fa03d97a30fd5abbfae311
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-04-23 21:28:32 +0530

    problem_solving

--- Problem_solving/problem_solving_7.c
+++ Problem_solving/problem_solving_7.c
@@ -1,16 +1,21 @@
-#include<stdio.h>
+// Write a c program to check whether a triangle is Equilateral, Isosceles or Scalene.
+
+#include <stdio.h>
 int main()
 {
-    int x,y,z;
+    int x, y, z;
     printf("Enter the sides of triangle\n");
-    scanf("\n%d\n%d\n%d",&x,&y,&z);
-    if (x==y==z)
+    scanf("\n%d\n%d\n%d", &x, &y, &z);
+    if (x == y == z)
     {
         printf("The triangle is equilateral");
     }
-    else if((x==y)||(y==z)||(x==z))
+    else if ((x == y) || (y == z) || (x == z))
     {
         printf("The triangle is isosceles");
     }
-    else{printf("The triangle is scalene");}
+    else
+    {
+        printf("The triangle is scalene");
+    }
 }
