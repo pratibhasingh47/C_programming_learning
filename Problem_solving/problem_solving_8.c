79fa572ee2213faa28d40483665599f693904b3b
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-04-26 13:30:27 +0530

    problem_solving

--- Problem_solving/problem_solving_8.c
+++ Problem_solving/problem_solving_8.c
@@ -1,10 +1,12 @@
-#include<stdio.h>
+// Write a program to swap values of two int variables without using third variable. For example if a=5 and b=10, then output should be: a= 10 and b=5.
+
+#include <stdio.h>
 
 int main()
 {
-    int x,y;
+    int x, y;
     printf("Enter the value\n");
-    scanf("%d\n%d",&x,&y);
-    
-    printf("After swapping\nx=%d\ny=%d",y,x);
+    scanf("%d\n%d", &x, &y);
+
+    printf("After swapping\nx=%d\ny=%d", y, x);
 }
