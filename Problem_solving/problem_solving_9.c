d4a417a5ad1778d12a1f39508ea2dacaa671d50b
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-04-26 13:31:23 +0530

    problem_solving

--- Problem_solving/problem_solving_9.c
+++ Problem_solving/problem_solving_9.c
@@ -1,21 +1,24 @@
-#include<stdio.h>
+// Write a program to print digits of one’s place and tenth’s place of a given number [For example, number is 2345, then digit at one’s place is 5 and digit at 10th place is 4]. Now check whether the digits at both place are equal or not.
+
+#include <stdio.h>
 int main()
 {
-    int x,y;
+    int x, y;
     printf("Enter the value\n");
-    scanf("\n%d",&x);
-    y=x%10;
-    printf("\nOnce digit is%d",y);
+    scanf("\n%d", &x);
+    y = x % 10;
+    printf("\nOnce digit is%d", y);
     int z;
-    z=(x%100)-y;
+    z = (x % 100) - y;
     int a;
-    a=z/10;
-    printf("\nTenth place is %d",a);
-    if (y==a)
+    a = z / 10;
+    printf("\nTenth place is %d", a);
+    if (y == a)
     {
         printf("\nThe digits at both the places is same");
     }
-    else{printf("\ndigits are not same");}
-    
-
+    else
+    {
+        printf("\ndigits are not same");
+    }
 }
