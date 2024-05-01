5649eba8b76ff5d8781c8562fb5ae048184558aa
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-01 20:24:23 +0530

    c

---
+++ Problem_solving & type_conversion/typeconversion_3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    float f;
+    printf("Testing of various arithmatic operations\n");
+    i=5/2.0;
+    f=5/2.0;
+    printf("\nvalue of integer i is %d",i);
+    printf("\nvalue of float f is %f",f);
+}
