1ce7379779f836969153f68d72642873109f9163
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-02 10:50:48 +0530

    update

---
+++ Problem_solving & type_conversion/typeconversion_10.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    float f;
+    printf("Testing of various arithmatic operations-2\n");
+    f = 2 / 9;
+
+    printf("\nvalue of integer i is %f", f);
+    f = 2.0 / 9;
+
+    printf("\nvalue of integer i is %f", f);
+    f = 2 / 9.0;
+
+    printf("\nvalue of integer i is %f", f);
+    f = 2.0 / 9.0;
+
+    printf("\nvalue of integer i is %f", f);
+    f = 9 / 2;
+
+    printf("\nvalue of integer i is %f", f);
+    f = 9.0 / 2;
+
+    printf("\nvalue of integer i is %f", f);
+    f = 9 / 2.0;
+
+    printf("\nvalue of integer i is %f", f);
+    f = 9.0 / 2.0;
+
+    printf("\nvalue of integer i is %f", f);
+}
