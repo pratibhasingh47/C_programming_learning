57ffa007c48834c62e4d8487b9218e1b300e2e7b
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-02 10:50:07 +0530

    update

---
+++ Problem_solving & type_conversion/typeconversion_9.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main ()
+{
+    int i;
+    printf("Testing of various arithmatic operations-2\n");
+    i=2/9;
+
+    printf("\nvalue of integer i is %d",i);
+    i=2.0/9;
+    
+    printf("\nvalue of integer i is %d",i);
+    i=2/9.0;
+    
+    printf("\nvalue of integer i is %d",i);
+    i=2.0/9.0;
+    
+    printf("\nvalue of integer i is %d",i);
+    i=9/2;
+    
+    printf("\nvalue of integer i is %d",i);
+    i=9.0/2;
+   
+    printf("\nvalue of integer i is %d",i);
+    i=9/2.0;
+    
+    printf("\nvalue of integer i is %d",i);
+    i=9.0/2.0;
+    
+    printf("\nvalue of integer i is %d",i);
+}
