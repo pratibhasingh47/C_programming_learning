2ee3eca1bcec3758f1476c50af7c80e4d3043580
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-03 21:29:09 +0530

    update.c

--- loops_basic/basic_loops_2.c
+++ loops_basic/basic_loops_2.c
@@ -1,12 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int n,i;
-    scanf("%d",&n);
-    for ( i = 1; i <= n; i++)
+    int n, i;
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
     {
-        printf("%d\n",i);
+        printf("%d\n", i);
     }
-    
-    
 }
