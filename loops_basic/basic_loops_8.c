87812e8598bafbbb29b0a30bd783ee6ad7b37133
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-06 00:34:26 +0530

    update.c

---
+++ loops_basic/basic_loops_8.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int x=20,i;
+    for ( i = 1; i <=x; i++)
+    {
+        if (i%2 != 0)
+        {
+            printf("%d\n",i);
+        }
+        
+    }
+    
+}
