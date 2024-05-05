e8b0edc7cf72ba7968da064253e58731bcd70424
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-05 20:35:27 +0530

    update.c

--- loops_basic/basic_loops_4.c
+++ loops_basic/basic_loops_4.c
@@ -1,20 +1,21 @@
-#include<stdio.h>
+// Write a program to calculate factorial of a number.
+
+#include <stdio.h>
 int factorial(int n);
 int main()
 {
-    int n,i;
+    int n, i;
     int ft;
-    scanf("%d",&n);
-    ft=factorial(n);
-    printf("\n%d",ft);
+    scanf("%d", &n);
+    ft = factorial(n);
+    printf("\n%d", ft);
 }
 int factorial(int n)
 {
-    int i,f=1;
-    for ( i = 1; i <=n; i++)
+    int i, f = 1;
+    for (i = 1; i <= n; i++)
     {
-        f=f*i;
-       
+        f = f * i;
     }
     return f;
 }
