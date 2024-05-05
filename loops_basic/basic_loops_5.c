32af916e29faee2d480d84e6eb867704e08806ad
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-05 20:39:30 +0530

    update.c

--- loops_basic/basic_loops_5.c
+++ loops_basic/basic_loops_5.c
@@ -1,10 +1,12 @@
-#include<stdio.h>
+// Write a program to make the last digit of a number stored in a variable as zero.  (Example - if num=5678 then make it num=5670)
+
+#include <stdio.h>
 int main()
 {
-    int n,y,z;
+    int n, y, z;
     printf("Enter the value\n");
-    scanf("%d",&n);
-    y=n%10;
-    z=n-y;
-    printf("\nThe value is %d",z);
+    scanf("%d", &n);
+    y = n % 10;
+    z = n - y;
+    printf("\nThe value is %d", z);
 }
