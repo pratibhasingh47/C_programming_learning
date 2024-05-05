536a2b74d3b81f02272480e6061ab9cf082e9bf3
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-05 20:40:52 +0530

    update.c

--- loops_basic/basic_loops_6.c
+++ loops_basic/basic_loops_6.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
+// WAP to print ‘10’ natural numbers in reverse order.
+
+#include <stdio.h>
 int main()
 {
-    int n=10,i;
-    for ( i = n; i <= n; i--)
+    int n = 10, i;
+    for (i = n; i <= n; i--)
     {
-        printf("%d\n",i);
-        if(i<=0)
-        break;
+        printf("%d\n", i);
+        if (i <= 0)
+            break;
     }
-    
 }
