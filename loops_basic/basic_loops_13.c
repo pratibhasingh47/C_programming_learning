8f7bc8c6c19e7a7ce8fda11d81a3683034a75f03
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-07 00:46:43 +0530

    c

--- loops_basic/basic_loops_13.c
+++ loops_basic/basic_loops_13.c
@@ -1,12 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int x=10,i;
-    for ( i = 1; i <= x; i++)
+    int x = 10, i;
+    for (i = 1; i <= x; i++)
     {
         int v;
-        v=i*i;
-        printf("%d\n",v);
+        v = i * i;
+        printf("%d\n", v);
     }
-    
 }
