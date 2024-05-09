c4600358d937b07dc787e9735705cbbf5ab55812
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-10 01:14:02 +0530

    update.c

---
+++ loops/for_loop_6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n=10,i;
+    for ( i = n; i <= n; i--)
+    {
+        printf("%d\n",i);
+        if(i<=0)
+        break;
+    }
+    
+}
---
+++ loops/for_loop_7.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n ,i;
+    scanf("%d",&n);
+    for ( i = n; i <= n; i--)
+    {
+        printf("%d\n",i);
+        if (i==1)
+        {
+            break;
+        }
+        
+    }
+    
+}
