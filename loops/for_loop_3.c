5dfe0ef6ad6f0e25a59b141bdb86dc22f12c5459
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-10 01:10:22 +0530

    update.c

---
+++ loops/for_loop_3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    for ( i = 1;i<=n; i++)
+    {
+        int x,y;
+        printf("\nEnter the values");
+        scanf("\n%d\n%d",&x,&y);
+        int sum;
+        sum=x+y;
+        printf("\nsum is %d",sum);
+    }
+    
+}
---
+++ loops/for_loop_4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int factorial(int n);
+int main()
+{
+    int n,i;
+    int ft;
+    scanf("%d",&n);
+    ft=factorial(n);
+    printf("\n%d",ft);
+}
+int factorial(int n)
+{
+    int i,f=1;
+    for ( i = 1; i <=n; i++)
+    {
+        f=f*i;
+       
+    }
+    return f;
+}
