3170a99fdb0b43ea4de8c7ba2fb2c8e23e74533c
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-10 01:14:29 +0530

    update.c

--- loops/for_loop_6.c
+++ loops/for_loop_6.c
@@ -1,12 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int n=10,i;
-    for ( i = n; i <= n; i--)
+    int n = 10, i;
+    for (i = n; i <= n; i--)
     {
-        printf("%d\n",i);
-        if(i<=0)
-        break;
+        printf("%d\n", i);
+        if (i <= 0)
+            break;
     }
-    
 }
--- loops/for_loop_7.c
+++ loops/for_loop_7.c
@@ -1,16 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int n ,i;
-    scanf("%d",&n);
-    for ( i = n; i <= n; i--)
+    int n, i;
+    scanf("%d", &n);
+    for (i = n; i <= n; i--)
     {
-        printf("%d\n",i);
-        if (i==1)
+        printf("%d\n", i);
+        if (i == 1)
         {
             break;
         }
-        
     }
-    
 }
