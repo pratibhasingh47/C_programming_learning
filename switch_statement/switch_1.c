68d93c9cc32fa874b9df619dd718d3a7ce0acf9c
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-11 00:09:18 +0530

    update.c

--- switch_statement/switch_1.c
+++ switch_statement/switch_1.c
@@ -1,59 +1,70 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     int x;
     printf("Enter the month no.\n");
-    scanf("%d",&x);
+    scanf("%d", &x);
     switch (x)
     {
-    case 1:{
+    case 1:
+    {
         printf("No. of days is 31");
         break;
     }
-    case 2:{
+    case 2:
+    {
         printf("No. of days is 28");
         break;
     }
-    case 3:{
+    case 3:
+    {
         printf("No. of days is 31");
         break;
     }
-    case 4:{
+    case 4:
+    {
         printf("No. of days is 30");
         break;
     }
-    case 5:{
+    case 5:
+    {
         printf("No. of days is 31");
         break;
     }
-    case 6:{
+    case 6:
+    {
         printf("No. of days is 30");
         break;
     }
-    case 7:{
+    case 7:
+    {
         printf("No. of days is 31");
         break;
     }
-    case 8:{
+    case 8:
+    {
         printf("NO. of days is 31");
         break;
     }
-    case 9:{
+    case 9:
+    {
         printf("No. of days is 30");
         break;
     }
-    case 10:{
+    case 10:
+    {
         printf("No. of days is 31");
         break;
     }
-    case 11:{
+    case 11:
+    {
         printf("No. of days id 30");
         break;
     }
-    case 12:{
+    case 12:
+    {
         printf("No. of days is 31");
         break;
     }
-    
     }
 }
--- switch_statement/switch_2.c
+++ switch_statement/switch_2.c
@@ -1,42 +1,47 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int a,b,x;
+    int a, b, x;
     printf("Select from the following options\n1. addition\n2. subtraction\n3. multiplication\n4. division\n5. exit programe\n");
-    scanf("%d",&x);
-    if (x==5)
+    scanf("%d", &x);
+    if (x == 5)
     {
         printf("EXIT");
     }
-    else {
-    printf("Enter the values");
-    scanf("%d\n%d",&a,&b);
-    switch (x)
+    else
     {
-    case 1:{
-        int add;
-        add=a+b;
-        printf("add is %d",add);
-        break;
-    }
-    case 2:{
-        int subt;
-        subt =a-b;
-        printf("Subt is %d",subt);
-        break;
-    }
-    case 3:{
-        int mlt;
-        mlt=a*b;
-        printf("multiplication is %d",mlt);
-        break;
-    }
-    case 4:{
-        int div;
-        div = a/b;
-        printf("division is %d",div);
-        break;
-    }
-    }
+        printf("Enter the values");
+        scanf("%d\n%d", &a, &b);
+        switch (x)
+        {
+        case 1:
+        {
+            int add;
+            add = a + b;
+            printf("add is %d", add);
+            break;
+        }
+        case 2:
+        {
+            int subt;
+            subt = a - b;
+            printf("Subt is %d", subt);
+            break;
+        }
+        case 3:
+        {
+            int mlt;
+            mlt = a * b;
+            printf("multiplication is %d", mlt);
+            break;
+        }
+        case 4:
+        {
+            int div;
+            div = a / b;
+            printf("division is %d", div);
+            break;
+        }
+        }
     }
 }
