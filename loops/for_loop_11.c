7a387b3236c9ad24eb23ca2ab308024824fda586
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-11 00:04:30 +0530

    update.c

--- loops/for_loop_11.c
+++ loops/for_loop_11.c
@@ -1,18 +1,17 @@
 
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int x=20,i;
-    for ( i = x; i <=x; i--)
+    int x = 20, i;
+    for (i = x; i <= x; i--)
     {
-        if (i%2 == 0)
+        if (i % 2 == 0)
         {
-            printf("%d\n",i);
+            printf("%d\n", i);
         }
-        if (i==1)
-            {
-                break;
-            }
+        if (i == 1)
+        {
+            break;
+        }
     }
-    
 }
