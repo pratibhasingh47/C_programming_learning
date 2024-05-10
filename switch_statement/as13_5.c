e79c299a94690748e6c9ef67476795623157807e
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-11 00:19:00 +0530

    c

---
+++ switch_statement/as13_5.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    printf("\nTo check:-\n1. Isoceles triangle\n2. Right angle triangle\n3.Equilateral triangle\n");
+    int n;
+    scanf("%d",&n);
+    switch (n)
+    {
+    case 1:{
+        
+            printf("Enter the sides of triangle\n");
+            scanf("%d\n%d\n%d",&x,&y,&z);
+            if (x==y!=z||y==z!=x||z==x!=y){
+            printf("\nsides are of isoceles triangle");
+        }
+        else
+        {printf("EXIT");}
+        break;
+    }
+    case 2:{
+        
+            printf("Enter the sides of triangle\n");
+            scanf("%d\n%d\n%d",&x,&y,&z);
+            if (x*x==y*y+z*z || y*y==x*x+z*z || z*z==x*x+y*y){
+            printf("\nsides are of right angled triangle");
+        }
+        else{printf("EXIT");}
+        break;
+    }
+    case 3:{
+            printf("Enter the sides of triangle\n");
+            scanf("%d\n%d\n%d",&x,&y,&z);
+            if (x==y&&y==z&&z==x){
+            printf("\nsides are of equilateral triangle");
+        }
+        else{printf("EXIT");}
+        break;
+    }
+    }
+}
