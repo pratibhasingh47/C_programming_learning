971139f7631580033b96e7341055e274e25adec5
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-11 00:09:45 +0530

    update.c

--- switch_statement/switch_3.c
+++ switch_statement/switch_3.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     int n;
     printf("Select from the followig food items:-\n1.Burger - 200Rs.\n2.French Fries - 50Rs.\n3.Pizza - 500Rs.\n4.Sandwiches - 150Rs.\n5.Enter some valid choice\n");
-    scanf("%d",&n);
-    if (n==5)
+    scanf("%d", &n);
+    if (n == 5)
     {
         printf("EXIT MENU");
     }
@@ -12,44 +12,46 @@ int main()
     {
         switch (n)
         {
-        case 1:{
+        case 1:
+        {
             printf("Enter the no. of quantity\n");
             int p;
-            scanf("%d",&p);
+            scanf("%d", &p);
             int t;
-            t=p*200;
-            printf("Total cost is %d",t);
+            t = p * 200;
+            printf("Total cost is %d", t);
             break;
         }
-        case 2:{
+        case 2:
+        {
             printf("Enter the no. of quantity\n");
             int x;
-            scanf("%d",&x);
+            scanf("%d", &x);
             int t;
-            t=50*x;
-            printf("Total cost is %d",t);
+            t = 50 * x;
+            printf("Total cost is %d", t);
             break;
         }
-        case 3:{
+        case 3:
+        {
             printf("Enter the no. of quantity\n");
             int y;
-            scanf("%d",&y);
+            scanf("%d", &y);
             int t;
-            t=y*500;
-            printf("Total cost is %d",t);
+            t = y * 500;
+            printf("Total cost is %d", t);
             break;
         }
-        case 4:{
+        case 4:
+        {
             printf("Enter the no. of quantity\n");
             int z;
-            scanf("%d",&z);
+            scanf("%d", &z);
             int t;
-            t=z*150;
-            printf("Total cost is %d",t);
+            t = z * 150;
+            printf("Total cost is %d", t);
             break;
         }
         }
-
     }
-    
 }
--- switch_statement/switch_4.c
+++ switch_statement/switch_4.c
@@ -1,42 +1,51 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     int n;
     printf("Enter the week no.\n");
-    scanf("%d",&n);
+    scanf("%d", &n);
     switch (n)
     {
-    case 1:{
+    case 1:
+    {
         printf("MONDAY\nYour attitude determines your behaviour");
         break;
     }
-    case 2:{
+    case 2:
+    {
         printf("TUESDAY\nMake your best decision today");
         break;
     }
-    case 3:{
+    case 3:
+    {
         printf("WEDNESDAY\nAs you grow you changes,as you changes you grow");
         break;
     }
-    case 4:{
+    case 4:
+    {
         printf("THURSDAY\nSpeed does not matter if you have a constant phase");
         break;
     }
-    case 5:{
+    case 5:
+    {
         printf("FRIDAY\nA new day,A new blessing,A new hope");
         break;
     }
-    case 6:{
+    case 6:
+    {
         printf("SATURDAY\nIt always seems impossible until its done");
         break;
     }
-    case 7:{
+    case 7:
+    {
         printf("SUNDAY\nRest buddy!!!");
         break;
     }
-    
+
     default:
-        {printf("EXIT");
-        break;}
+    {
+        printf("EXIT");
+        break;
+    }
     }
 }
